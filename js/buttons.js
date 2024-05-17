5e87ef8abe64907a82010b348703c004b659205e
Valentin-Franyo <valentinfranyo@gmail.com>
2024-05-17 13:09:47 +0200

    portfolio oneclick

--- js/buttons.js
+++ js/buttons.js
@@ -8,6 +8,11 @@ function BlogHU() {
     window.location.href = "blogHU.html"
 }
 
+// portfolio oldalra
+function PortfolioHU() {
+    window.location.href = "portfolioHU.html"
+}
+
 //tudorak diagnosztika
 function scrollToProject() {
     var projectTitle = document.querySelector("#project-page__h2js");
