12e4f9aef0244fce68ea2c181674ef23e500dc87
Valentin-Franyo <valentinfranyo@gmail.com>
2024-06-04 01:54:14 +0200

    portfolio download function

--- js/buttons.js
+++ js/buttons.js
@@ -18,3 +18,9 @@ function scrollToProject() {
     var projectTitle = document.querySelector("#project-page__h2js");
     projectTitle.scrollIntoView({ behavior: "smooth" });
 }
+
+
+// portfolio letoltes
+document.getElementById('portfolioDownload').addEventListener('click', function() {
+    window.open('/assets/portfolio_download/DSE_HU.pdf', '_blank');
+});
